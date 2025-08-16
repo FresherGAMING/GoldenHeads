03f66054cef1cddc13f00952052f9287e6acafa0
FresherGAMING <87463272+FresherGAMING@users.noreply.github.com>
2025-08-17 06:57:41 +0700

    Create EventListener.php

---
+++ src/FresherGAMING/GoldenHeads/EventListener.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace FresherGAMING\GoldenHeads;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerItemUseEvent;
+
+class EventListener extends Listener {
+
+  public function onUse(PlayerItemUseEvent $event){
+    $p = $event->getPlayer();
+    $item = $event->getItem();
+    //if(){
+      
+    //}
+  }
+}
