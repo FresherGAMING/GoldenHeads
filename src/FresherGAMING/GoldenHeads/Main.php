ddc114c4a15d7b273d67522f5ff3c3e661a28ec7
FresherGAMING <87463272+FresherGAMING@users.noreply.github.com>
2025-08-17 06:49:45 +0700

    Update Main.php

--- src/FresherGAMING/GoldenHeads/Main.php
+++ src/FresherGAMING/GoldenHeads/Main.php
@@ -4,8 +4,20 @@ namespace FresherGAMING\GoldenHeads;
 
 use pocketmine\plugin\PluginBase;
 
+use customiesdevs\customies\block\CustomiesBlockFactory;
+
 class GoldenHeads extends PluginBase {
   
-  public function onEnable() : void {}
+  public function onEnable() : void {
+    CustomiesBlockFactory::getInstance()->registerBlock(
+      static fn(int $id) => 
+        new Block(
+          new BlockIdentifier($id, 0),
+          "Golden Heads",
+          new BlockBreakInfo(1)
+        ),
+        "minecraft:golden_heads
+    );
+  }
 
 }
